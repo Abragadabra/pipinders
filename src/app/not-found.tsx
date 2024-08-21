34e7fd6915d979958c3e5482020dcd820f0fdd3b
Daniil <baldindaniilza@mail.ru>
2024-08-22 00:27:51 +0500

    Я пытался Видео не але(

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,11 @@ export default function NotFound() {
 					Вы ошиблись адресом
 				</h2>
 				<div className='flex items-center justify-center mt-5'>
-					<Image src='/404.jpg' width={300} height={300} alt='404 cat' />
+					{/* <Image src='/404.jpg' width={300} height={300} alt='404 cat' /> */}
+					<video autoPlay playsInline loop controls>
+						<source src='/404.mp4' type='video/mp4' />
+						Your browser does not support the video tag.
+					</video>
 				</div>
 			</div>
 		</section>
